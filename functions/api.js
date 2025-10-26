63f6c31727a3e4913ddebe4290c75c1c669f5bf6
DiqySH <margolang55@gmail.com>
2025-10-26 17:00:38 +0700

    testing - 4

--- functions/api.js
+++ functions/api.js
@@ -3,6 +3,7 @@ import { connectDB } from "../src/db/connection.js";
 import dotenv from "dotenv";
 import notesRoutes from "../src/routes/notesRoutes.js";
 import ServerlessHttp from "serverless-http";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,12 +11,14 @@ const app = express();
 
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/notes", notesRoutes);
 
-// app.listen(PORT, () => {
-//   console.log("SERVER STARTED ON PORT:", PORT);
-// });
+const handlerWrapper = ServerlessHttp(app);
 
-export const handler = ServerlessHttp(app);
+export const handler = async (event, context) => {
+  const result = await handlerWrapper(event, context);
+  return result;
+};
