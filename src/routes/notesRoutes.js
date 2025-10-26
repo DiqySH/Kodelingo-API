62a80f78f9fd1a848dbaf52faf85aca70fa036fa
DiqySH <margolang55@gmail.com>
2025-10-26 21:14:23 +0700

    testing - 6

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { getAllNotes, createNote } from "../controllers/notesController.js";
+import {
+  getAllNotes,
+  createNote,
+  updateNote,
+} from "../controllers/notesController.js";
 
 const router = express.Router();
 
 router.get("/", getAllNotes);
 router.post("/", createNote);
-router.put("/:id", getAllNotes);
+router.put("/:id", updateNote);
 router.delete("/:id", getAllNotes);
 
 export default router;
