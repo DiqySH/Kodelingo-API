4f55e4ebdb008d5862cabd34fbb663838fd8658e
DiqySH <margolang55@gmail.com>
2025-11-03 11:18:14 +0700

    testing - 16

--- src/models/City.js
+++ src/models/City.js
@@ -4,7 +4,6 @@ const citySchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   description: String,
   background: String,
-  music: String,
   levels: [
     {
       id: Number,
