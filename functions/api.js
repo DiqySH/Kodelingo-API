38925de20d78b232c339bb1a4fa6c226c44fedee
DiqySH <margolang55@gmail.com>
2025-11-03 20:12:36 +0700

    testing - 19

--- functions/api.js
+++ functions/api.js
@@ -14,7 +14,8 @@ const app = express();
 connectDB();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use("/api/admins", adminsRoutes);
 app.use("/api/entities", entityRoutes);
