d24d6756dfdb969e99fde1ff904e9adce95a2051
DiqySH <margolang55@gmail.com>
2025-11-04 11:25:40 +0700

    testing - 22

--- src/models/City.js
+++ src/models/City.js
@@ -4,35 +4,20 @@ const questionSchema = new mongoose.Schema({
   text: { type: String, required: true },
   options: [String],
   answer: { type: String, required: true },
-  type: { type: String, default: "multiple_choice" },
+  type: {
+    type: String,
+    enum: ["multiple_choice", "true_false", "fill_blank", "code_snippet"],
+    default: "multiple_choice",
+  },
 });
 
 const levelSchema = new mongoose.Schema({
   id: { type: Number, required: true },
-  entityName: { type: String, required: true }, 
-  type: { type: String, enum: ["normal", "boss"], default: "normal" },
-  
-  question: {
-    type: questionSchema,
-    required: function () {
-      return this.type === "normal";
-    },
-  },
-
-  questions: {
-    type: [questionSchema],
-    required: function () {
-      return this.type === "boss";
-    },
-    default: undefined,
-  },
-
-  hp: {
-    type: Number,
-    default: function () {
-      return this.type === "boss" ? 100 : 50;
-    },
-  },
+  entityName: { type: String, required: true },
+  type: { type: String, enum: ["enemy", "boss"], required: true },
+  hp: { type: Number, required: true },
+  attackPower: { type: Number, required: true },
+  questions: { type: [questionSchema], required: true },
 });
 
 const citySchema = new mongoose.Schema({
