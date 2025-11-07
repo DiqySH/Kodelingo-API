4bce08a125a8638fc697750f92d8bd6532e7de39
DiqySH <margolang55@gmail.com>
2025-11-07 15:40:00 +0700

    testing - 27

--- functions/api.js
+++ functions/api.js
@@ -1,11 +1,7 @@
 import express from "express";
 import { connectDB } from "../src/db/connection.js";
 import dotenv from "dotenv";
-import adminsRoutes from "../src/routes/adminsRoutes.js";
 import ServerlessHttp from "serverless-http";
-import entityRoutes from "../src/routes/entityRoutes.js";
-import cityRoutes from "../src/routes/cityRoutes.js";
-import uploadRoutes from "../src/routes/uploadRoutes.js";
 import cors from "cors";
 
 dotenv.config();
@@ -16,10 +12,6 @@ connectDB();
 
 app.use(cors());
 
-// app.use("/api/admins", adminsRoutes);
-// app.use("/api/entities", entityRoutes);
-// app.use("/api/cities", cityRoutes);
-// app.use("/api/upload", uploadRoutes);
 app.use("/api", async (req, res) => {
   return res.status(200).json({
     message: "HELLO!",
